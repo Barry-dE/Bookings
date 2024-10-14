07dd2281d17389b52a7f94d521f1b555b5fc3339
Barigbue <barigbuenbira@gmail.com>
2024-10-14 03:28:48 +0100

    Reduce header font size for all pages

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -4,24 +4,23 @@ const Heading = styled.h1`
   ${(props) =>
     props.as === "h1" &&
     css`
-      font-size: 3rem;
+      font-size: 2rem;
       font-weight: 600;
     `}
 
   ${(props) =>
     props.as === "h2" &&
     css`
-      font-size: 2rem;
+      font-size: 1.5rem;
       font-weight: 600;
     `}
 
    ${(props) =>
     props.as === "h3" &&
     css`
-      font-size: 2rem;
+      font-size: 1rem;
       font-weight: 500;
-    `}
-  background-color: yellow;
+    `} /* background-color: yellow; */
 `;
 
 export default Heading;
